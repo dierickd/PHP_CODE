34db1bc39332241184a9ef075d486b0a77ed9370
dierickd <37022827+dierickd@users.noreply.github.com>
2020-04-18 13:30:30 +0200

    Update back_to_the_futur.php

--- back_to_the_futur.php
+++ back_to_the_futur.php
@@ -13,7 +13,7 @@ echo '<br>';
 echo $interval->format('%Y') . ' year(s) ' . $interval->format('%M') . ' month(s) ' . $interval->format('%D');
 echo ' day(s) ' . $interval->format('%H') . ' hour(s) ' . $interval->format('%I') . ' minute(s)';
 
-echo '<br><br>La quantité necessaire est d\'environ 0.2% pour 1 an de voyage dans le temps<br><br>';
+echo '<br><br>Calcul du carburant<br><br>';
 
 if ($interval->format('%a') * 0.02 > 100) {
    echo '<b>Impossible d\'effectuer ce voyage temporel, tu n\'a pas suffisamment de carburant !<b>';
